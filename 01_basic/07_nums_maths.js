ac687f523688c004203af8dea36643783a9f853d
sudipta khotel <121477770+sudiptakhotel@users.noreply.github.com>
2023-12-27 08:04:11 +0000

    nums_commit

---
+++ 01_basic/07_nums_maths.js
@@ -0,0 +1,21 @@
+//Here we are going to explore Number and Math library of js
+
+//Number --> There is 2 ways to declare a Number
+//1st
+// let myAge = 23; //type - number
+// //2nd
+// let myNewAge = new Number(23); // Here we are specifying that we only want Number by using Number object . type - object
+// console.log(typeof(myAge));
+// console.log(typeof(myNewAge));
+
+//+++++++++++++++++++++++FUNCTIONS++++++++++++++++++++++++
+
+//lets explore with some functions of Numbers
+
+//toExponential() --> this functions returns a String by converting input number in exponential format.
+
+// let number1 = 1567;
+// console.log(number1.toExponential()); //o/p --> 1.567e+3
+
+//--------------------------------------------
+//toFixed() --> 
