9a017a62020b4132dc0cb2c7411ec5bf7d607d67
sudipta khotel <121477770+sudiptakhotel@users.noreply.github.com>
2023-12-29 07:20:30 +0000

    this and arrow function

---
+++ 03_basic/03_this.js
@@ -0,0 +1,25 @@
+// //this keyword is use to access the current context
+
+// const user = {
+//     userName : "Sudipta khotel",
+//     userId : 1245667,
+
+//     address : {
+//         state : "West Bengal",
+//         city : "Howrah" ,
+//         pinCode : 7111011,
+
+//         currentLocation : {
+//             landMark : "Mansatala",
+//         }
+//     },
+
+//     //lets print all the details
+//     welcomeMessage : function (){
+
+//         console.log(`Hi ${this.userName} , please find your details --> userId : ${this.userId} , city : ${this.address.city} , landMark : ${this.address.currentLocation.landMark}`);
+
+//     }
+// }
+
+// user.welcomeMessage();
---
+++ 03_basic/04_arrow_function.js
@@ -0,0 +1,58 @@
+//here we wra going to see arrow functions and different ways to declare arrow function
+
+//declaration -
+
+//  const display = () =>  {
+//     console.log("Hi");
+// } 
+// display();
+
+// const addTwoNumber = (num1 , num2) => (num1 + num2);
+
+// console.log(addTwoNumber(5,7));
+
+//--------------------------------
+
+// function objDisplay() {
+   
+//     let myUser = {
+//         userName : "sudipta",
+//         empId : 1215420
+//     };
+//     console.log(myUser);
+
+// }
+// objDisplay();
+
+//lets make this objDisplay() by arrow funtion
+
+// const objDisplay = () => {
+//     const myObject = {
+//         userName : "rahul",
+//         empId : 1215423
+
+//     }
+//     return myObject;
+// }
+
+// console.log(objDisplay());
+
+//lets make more arrow function
+
+
+// const objInfo = () => {
+
+//     let myObj = {
+//         userName : "sudipta",
+//         empId : 124578,
+
+//         welcomeMessage : () =>{
+//             console.log(`Hi ${myObj.userName} welcome to website`);
+//         }
+//     }
+//     myObj.welcomeMessage();
+// }
+// console.log(myObj.userName); myObj is not in the scope
+
+//++++++++++++++++++++++++++++++++++
+
