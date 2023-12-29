cff5e7a5a0d2428fa9e0d6e71f4f2b6a9e76bba6
sudipta khotel <121477770+sudiptakhotel@users.noreply.github.com>
2023-12-29 18:40:16 +0000

    for loop added

---
+++ 04_iteration/01_for_loop.js
@@ -0,0 +1,31 @@
+//basic for loop concept
+
+// for (let i = 1; i <= 10; i++) {
+    
+//     console.log(`Table of ${i}`);
+//     for(let j = 1; j <= 10 ; j++){
+        
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+//break and coninue
+
+//break - > it breaks the flow and comes out of scope
+//continue -> it skips the execution for that round and starts from the next round
+
+// for(let i = 1 ; i<5 ; i++){
+//     if(i == 4){
+//         console.log(`${i} detected`);
+//         break;
+//     }
+//     console.log(`value is ${i}`);
+// }
+
+// for(let i = 1 ; i<=5 ; i++){
+//     if(i == 4){
+//         console.log(`${i} detected`);
+//         continue;
+//     }
+//     console.log(`value is ${i}`);
+// }
