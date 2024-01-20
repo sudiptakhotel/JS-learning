1ec3cbdcb84c42d50a206426ad5fb255b2de25ce
sudipta khotel <121477770+sudiptakhotel@users.noreply.github.com>
2024-01-20 19:29:17 +0000

    prototyoe , new added

---
+++ 08_OOPS/three.js
@@ -0,0 +1,62 @@
+//here we are going to see prototype , inheritance and how js is using sugar coating by giving class , inheritence or other OOPS concept
+
+//so as we know everything in js is an object and also we know object has protype which is basically use in inheritance
+
+//We can set any property or method inside prototype of any objet such as Array , String , function etc.
+
+//Everything is pass from Object in js so if we create an array , string or function or any other object by default they have the access of Object's prototype
+
+//We can create and inject any propety or method to any object's prototype
+
+//I want to add a method in String object which will be available whenever I create any String
+
+//I will go inside the prototype of String and declare me method
+
+// String.prototype.display = function(){
+//     //here this is used to give current context that means "jisne bulaye unko"
+//     console.log(`The string is ${this}`);
+// }
+
+// const myName = "Rahul";
+// myName.display(); //this is available
+
+//Inheritance - > parent child property
+
+//we can by pass any other object's property to other by 
+//old ways - __proto__
+
+const user = {
+    userName : "user@user.com",
+    password : 12345,
+
+    getDetails : function(){
+        console.log(`userName : ${this.userName} , passWord : ${this.password}`);
+    }
+};
+
+const guest = {
+    isGuest : true,
+    //__proto__ : user
+}
+const signedIn = {
+    isSignedIn : true,
+    //__proto__ : user
+}
+
+//if I want my guest and signedIn can get access of the properties of user
+
+ //guest.getDetails(); //getDetails is not defined inside guest
+
+ //modern way of inheritance - setPrototypeOf()
+
+//  Object.setPrototypeOf(guest , user);
+//  guest.getDetails();
+
+// Object.prototype.hello = function(){
+//     console.log("Hello");
+// }
+
+// const myArr = [1,23,45];
+// myArr.hello();
+// "rahul".hello();
+
---
+++ 08_OOPS/two.js
@@ -0,0 +1,27 @@
+//here we are going to see the power of new keyword in js . 
+//we will write a constructor fucntion and by using new we will create object of the constructor function 
+
+function createPlayer(playerName , jersey){
+    this.playerName = playerName;
+    this.jersey = jersey;
+
+    this.getDetails = function(){
+        console.log(`player : ${this.playerName} jersey : ${this.jersey}`);
+    }
+}
+
+//now as we know function is also an object in js so we will get its prototype as other object
+
+//so I want to add some method into the prototype of the createPlayer() so that other instances which is created by using new can use them
+
+//this showMe() is a shared method and can be used by other instances of createPlayer() , because when we intialize a function by new keyword new make linking of newly created object's prototype to it's parent means here createPlayer() prototype . In this way protypy chaining or inheritence extablishes so newly created object get access of the createPlayer() properties and method and properties or method which are injected inside protptype.
+
+createPlayer.prototype.showMe = function(){
+    console.log(`Player jersey number is : ${this.jersey}`);
+}
+
+//creating instance of the createPlayer()
+const player1 = new createPlayer("Lionel Messi" , 10);
+console.log(player1.getDetails());
+console.log(player1.showMe());
+
